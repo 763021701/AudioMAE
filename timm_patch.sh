215e4728f27d091050a1142dbc1f5f64ca52bce6
huangpu <763021701@qq.com>
2024-12-27 06:20:04 +0000

    Fix bugs for ttt_mae env
    Signed-off-by: huangpu <763021701@qq.com>

--- timm_patch.sh
+++ timm_patch.sh
@@ -1,3 +1,3 @@
-timm_path=timm_path=/home/ubuntu/miniconda3/envs/audio_mae/lib/python3.8/site-packages/timm/models/
+timm_path=/home/ubuntu/miniconda3/envs/ttt_mae/lib/python3.10/site-packages/timm/models/
 cp timm_patch/swin_transformer.py $timm_path
 cp timm_patch/helpers.py $timm_path/layers/
